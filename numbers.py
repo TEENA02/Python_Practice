1c20a4de16cf61af4e6887ee2ae502ca031e48fb
TEENA02 <teenasajwan09@gmail.com>
2026-01-29 13:59:48 +0530

    numbers

---
+++ numbers.py
@@ -0,0 +1,10 @@
+# numbers is group of immutable data types , boolean is also included in this group , sets also partially immutable as its elements must be immutable
+# immutable data types : int , float , complex , bool , string , tuple , frozenset
+# mutable data types : list , set , dict
+# immutable data types cannot be changed once created , any operation on immutable data types creates a new object in memory
+
+a=10
+b=7.8
+print(a+b)
+str="demo"
+print(str+9)
