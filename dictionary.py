a5cce0cd2c7b8697ac37d1edae38b7bd73bdf220
TEENA02 <teenasajwan09@gmail.com>
2026-02-06 14:02:46 +0530

    loops in dictionary

--- dictionary.py
+++ dictionary.py
@@ -5,3 +5,8 @@ thisdisc={
 }
 print(thisdisc["model"])
 print(thisdisc.get("brand"))
+thisdisc['year']=2020
+print(thisdisc)
+for x in thisdisc:
+    # print(x,end=" | ")
+    print(thisdisc[x],end=",")
