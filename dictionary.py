f038ac99b333f039adf50672d4befd6defe7f35d
TEENA02 <teenasajwan09@gmail.com>
2026-02-06 14:09:18 +0530

    more about dictionary

--- dictionary.py
+++ dictionary.py
@@ -7,6 +7,24 @@ print(thisdisc["model"])
 print(thisdisc.get("brand"))
 thisdisc['year']=2020
 print(thisdisc)
-for x in thisdisc:
+# for x in thisdisc:
+#     # print(x,end=" | ")
+#     print(thisdisc[x],end=",")
+
+for x,y in thisdisc.items():
     # print(x,end=" | ")
-    print(thisdisc[x],end=",")
+    print(x,y,end=",")
+if "brand" in thisdisc:
+    print("\nBrand is present in the dictionary")
+thisdisc["color"]="red"
+thisdisc["price"]=55000
+print(thisdisc)
+print(thisdisc.pop("brand")
+)
+print(thisdisc.popitem())
+# Ford
+# ('price', 55000)
+del thisdisc["model"]
+print(thisdisc)
+cardetails=thisdisc.copy()
+print(cardetails)
