62d792d23c03f515010ae1ac412c7373bf0e1e15
TEENA02 <teenasajwan09@gmail.com>
2026-02-16 13:48:50 +0530

    Scope LEGB

---
+++ scope/scope.py
@@ -0,0 +1,15 @@
+username="teena sajwan"
+def print_username():
+    # username="teena"
+    print(username)
+print(username)
+print_username()
+
+x=100
+def print_x():
+    global x
+    x=1001
+
+print(x)
+print_x()
+print(x)
