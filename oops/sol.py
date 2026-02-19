3b181f9e070f2b7fdb5a469b52c3dfeb0baaa83a
TEENA02 <teenasajwan09@gmail.com>
2026-02-19 12:54:34 +0530

    oops inheritance

--- oops/sol.py
+++ oops/sol.py
@@ -5,3 +5,18 @@ class Car:
     def display(self):
         print("Brand:", self.Brand)
         print("Model:", self.Model)
+
+car1=Car("hunda", "civic")
+car1.display()
+
+# inheritance
+class ElectricCar(Car):
+    def __init__(self, Brand, Model, BatterySize):
+        super().__init__(Brand, Model)
+        self.BatterySize = BatterySize
+    def display(self):
+        super().display()
+        print("Battery Size:", self.BatterySize)
+ElectricCar1=ElectricCar("tesla","xqwe",100
+                         )
+ElectricCar1.display()
